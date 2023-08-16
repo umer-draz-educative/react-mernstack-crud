dadc857d7c9e0b65d433cca658f2cb976ddbd443
umer-draz-educative <142388993+umer-draz-educative@users.noreply.github.com>
2023-08-16 14:31:24 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use('/students', studentRoute)
 
 
 // PORT
-const port = process.env.PORT || 4000;
+const port =  4000;
 const server = app.listen(port, () => {
   console.log('Connected to port ' + port)
 })
