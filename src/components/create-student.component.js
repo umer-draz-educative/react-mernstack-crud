fe910b223b7eef768127ca4beca118a84fe52326
umer-draz-educative <142388993+umer-draz-educative@users.noreply.github.com>
2023-08-16 16:01:22 +0500

    Update create-student.component.js

--- src/components/create-student.component.js
+++ src/components/create-student.component.js
@@ -42,7 +42,7 @@ export default class CreateStudent extends Component {
       email: this.state.email,
       rollno: this.state.rollno
     };
-    axios.post('0.0.0.0:3000/students/create-student', studentObject)
+    axios.post('students/create-student', studentObject)
       .then(res => console.log(res.data));
 
     this.setState({ name: '', email: '', rollno: '' })
